9a7fac4d392a53ee6855fac7e0f018a37726353e
antonhtmlito <antondong04@gmail.com>
2025-06-09 18:53:27 +0200

    add calibrate yellow ball

--- detect_white_and_yellow_ball.py
+++ detect_white_and_yellow_ball.py
@@ -47,7 +47,39 @@ def get_color(event, x, y, flags, param):
         object_configs[1]["colorLowerBound"] = lower.tolist()
         #object_configs[1]["colorUpperBound"] = upper.tolist()
         print(object_configs[1])
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        print(frame[y][x])
+        tempFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mouseClickColour = tempFrame[y][x]
+        print(mouseClickColour)
+        lower = mouseClickColour.copy()
+        # below mapping is to ensure that the lower bound is not wrapped to the highest value
+        hueChange = 12
+        SaturationChange = 40
+        ValueChange = 75
 
+        if lower[0] < hueChange:
+            lower[0] = hueChange + 1
+        if lower[1] < SaturationChange:
+            lower[1] = SaturationChange + 1
+        if lower[2] < ValueChange:
+            lower[2] = ValueChange + 1
+        np.subtract.at(lower, [0, 1, 2], [hueChange, SaturationChange, ValueChange])
+
+        upper = mouseClickColour.copy()
+        # upper mapping is to ensure that the upper bound is not wrapped to the lowest value
+        if upper[0] > 255 - hueChange:
+            upper[0] = 255 - hueChange - 1
+        if upper[1] > 255 - SaturationChange:
+            upper[1] = 255 - SaturationChange - 1
+        if upper[2] > 255 - ValueChange:
+            upper[2] = 255 - ValueChange - 1
+        np.add.at(upper, [0, 1, 2], [hueChange, SaturationChange, ValueChange])
+        np.clip(upper[0], 0, 180)
+
+        object_configs[0]["colorLowerBound"] = lower.tolist()
+        #object_configs[0]["colorUpperBound"] = upper.tolist()
+        print(object_configs[0])
 
 
 # ──────────────────────────────────────────────────────────────────────────────
