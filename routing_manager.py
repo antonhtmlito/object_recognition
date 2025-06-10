ea13d5e29fac2789645459b32ab706612679c4d9
jonaswoetmann <jonas.woetmann@gmail.com>
2025-06-10 17:36:53 +0200

    .

--- routing_manager.py
+++ routing_manager.py
@@ -30,13 +30,7 @@ def handle_routing(player, obstacle, goal, roboController):
         ]
         for target in nearby_targets:
             routing_functions.all_targets.remove(target)
-        if nearby_targets:
-            routing_functions.calculate_target()
-            tx, ty = routing_functions.target_x, routing_functions.target_y
-            if tx is not None and ty is not None:
-                routing_functions.target_x, routing_functions.target_y = routing_functions.avoid_walls(tx, ty)
-        else:
-            routing_functions.calculate_target()
+        routing_functions.calculate_target()
 # Drive to target
     angle_to_turn = routing_functions.calculate_angle(routing_functions.target_x, routing_functions.target_y)
     # print("angle to turn: ", angle_to_turn)
