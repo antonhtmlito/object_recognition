27b8c9303cfddd70379797392718e9fec7cafd51
antonhtmlito <antondong04@gmail.com>
2025-06-24 16:17:41 +0200

    bugfixes

--- sounds.py
+++ sounds.py
@@ -20,11 +20,15 @@ def play_sound_thread(text=None, tones=None):
 def play_music_file(file_path, volume=100):
     def _play_music():
         if sound is None:
-            print(f"[Simulated Playback] Playing {file_path}")
+            print(f"[Simulated Playback] Playing {file_path} at volume{volume}")
             return
         try:
             sound.volume = volume
-            sound.play_file(file_path, play_type=Sound.PLAY_WAIT_FOR_COMPLETE)
+            sound.play_file(
+                file_path,
+                volume=volume,
+                play_type=Sound.PLAY_NO_WAIT_FOR_COMPLETE
+            )
         except Exception as e:
-            print(f"Error playing file: {e}")
+            print(f"Error playing file {file_path}: {e}")
     threading.Thread(target=_play_music, daemon=True).start()
