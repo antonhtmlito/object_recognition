d2e9f6d97c1fa9fee5441a87df3e551ca6f1a600
Lars Nissen <larsnissen@larss-mbp.students.clients.local>
2025-06-24 15:24:03 +0200

    fix

--- routingController.py
+++ routingController.py
@@ -123,13 +123,17 @@ class RoutingController:
             if angle < 0:
                 print("rotate counter") if DEBUG_ROUTING else None
                 if self.roboController.driving is True:
-                    self.roboController.drivestop()
-                self.roboController.rotate_counterClockwise(abs(angle))
+                    if self.getDistanceToCurrentTarget() < 400:
+                        self.roboController.drivestop()
+                elif self.roboController.driving is False:
+                    self.roboController.rotate_counterClockwise(abs(angle))
             elif angle > 0:
                 print("rotate") if DEBUG_ROUTING else None
                 if self.roboController.driving is True:
-                    self.roboController.drivestop()
-                self.roboController.rotate_clockwise(abs(angle))
+                    if self.getDistanceToCurrentTarget() < 400:
+                        self.roboController.drivestop()
+                elif self.roboController.driving is False:
+                    self.roboController.rotate_clockwise(abs(angle))
             else:
                 raise Exception("Angle to turn somehow zero though it did not drive")
 
