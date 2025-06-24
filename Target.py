07930b4793cc2c3f2339e6b007338a4af45248a0
Lars Nissen <larsnissen@larss-mbp.students.clients.local>
2025-06-24 10:55:55 +0200

    fix

--- Target.py
+++ Target.py
@@ -48,7 +48,7 @@ class Target:
             mask=self.mask,
         )
         hit_directions = []
-        directions = {0: "s", 270: "w", 180: "n", 90: "e"}
+        directions = {270: "s", 180: "w", 90: "n", 0: "e"}
 
         for angle, hit in zip(directions.keys(), ray_results):
             if hit is not None:
