94902a01e17333431504fc602682f29c4cdbccbc
Lars Nissen <larsnissen@larss-mbp.students.clients.local>
2025-06-24 15:07:41 +0200

    fix

--- routingController.py
+++ routingController.py
@@ -115,7 +115,7 @@ class RoutingController:
                     self.roboController.drivestart(speed=5)
             elif self.currentTarget.approach_angle() is None:
                 distance = self.getDistanceToCurrentTarget()
-                speed = distance*0.05+5
+                speed = distance*0.1+5
                 self.roboController.drivestart(speed = speed)
 
 
