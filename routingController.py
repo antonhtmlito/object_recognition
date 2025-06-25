0d3336c0c653cff94ba5565d9666a0010d03b884
Lars Nissen <larsnissen@larss-mbp.students.clients.local>
2025-06-25 11:26:55 +0200

    fix

--- routingController.py
+++ routingController.py
@@ -101,6 +101,10 @@ class RoutingController:
             else:
                 print("target", self.currentTarget) if DEBUG_ROUTING else None
                 print("approach: ", approach) if DEBUG_ROUTING else None
+            
+        angle = self.getAngleToCurrentTarget()
+        if angle is None:
+            return
 
         print("angle to rotate", angle) if DEBUG_ROUTING else None
         hit = self.checkCollisionsForAngle(angle=angle["angleTarget"]) # for now angle is not used as it is defined elsewhere
